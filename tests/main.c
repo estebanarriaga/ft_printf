bfc039f33c703d6f0371359203476b8889b1c6c9
Esteban Arriaga Vijande <earriaga@c1r5s6.42madrid.com>
2023-09-05 17:05:25 +0200

    New tests

--- tests/main.c
+++ tests/main.c
@@ -1,20 +1,32 @@
 
 #include "../libftprintf.h"
+#include <assert.h>
+#include <sys/time.h>
 
 int	main(void)
 {
-	int var = 12399;
-	char chr = 'n';
-	char name[] = "Antonio";
-	int *ptr;
-	int ft_size;
-	int or_size;
+	char c = 'A';
+	char *str = "This happens when your program reads or writes memory at a place which Memcheck reckons it shouldn't. In this example, the program did a 4-byte read at address 0xBFFFF0E0, somewhere within the system-supplied library libpng.so.2.1.0.9, which was called from somewhere else in the same library, called from line 326 of qpngio.cpp, and so on.Memcheck tries to establish what the illegal address might relate to, since that's often useful. So, if it points into a block of memory which has already been freed, you'll be informed of this, and also where the block was freed. Likewise, if it should turn out to be just off the end of a heap block, a common result of off-by-one-errors in array subscripting, you'll be informed of this fact, and also where the block was allocated. If you use the --read-var-info option Memcheck will run more slowly but may give a more detailed description of any illegal address.";
+	int num = 42;
+	// ASSERTION TESTING
+	assert(ft_printf("Character: %c\n", 'A') == printf("Character: A\n"));
+	assert(ft_printf("String: %s\n", str) == printf("String: %s\n", str));
+	assert(ft_printf("Integer: %i\n", num) == printf("Integer: %i\n", num));
+	assert(ft_printf("Escaping %%: %%\n") == printf("Escaping %%: %%\n"));
+	printf("All test cases passed!\n");
 
-	ft_size = ft_printf("Tengo %x armas, %s\n", var, name);
-	or_size = printf("Tengo %x armas, %s\n", var, name);
+	// MEMORY TESTING.
+	 
 
-	printf("ft_size: %d\n", ft_size);
-	printf("or_size: %d\n", or_size);
+	// PERFORMANCE TESTING:
+/* 	struct timeval t0, t1;
+	unsigned int i;
 
-	return (0);
+	gettimeofday(&t0, NULL);
+	for(i = 0; i < 100000; i++)
+		function_to_measure();
+	gettimeofday(&t1, NULL);
+	printf("Did %u calls in %.2g seconds\n", i, t1.tv_sec - t0.tv_sec + 1E-6 * (t1.tv_usec - t0.tv_usec));
+ */
+	return (0);	
 }
