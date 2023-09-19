55d6760fc965c9f5e93aaa088a2dc5f24441524d
estebanarriaga <estebanarriaga200@gmail.com>
2023-09-19 06:23:23 +0200

    Added argv in main

--- tests/main.c
+++ tests/main.c
@@ -3,7 +3,7 @@
 #include <assert.h>
 #include <sys/time.h>
 
-int	main(void)
+int	main(int argc, char **argv)
 {
 	char c = 'A';
 	char *str = "This happens when your program reads or writes memory at a place which Memcheck reckons it shouldn't. In this example, the program did a 4-byte read at address 0xBFFFF0E0, somewhere within the system-supplied library libpng.so.2.1.0.9, which was called from somewhere else in the same library, called from line 326 of qpngio.cpp, and so on.Memcheck tries to establish what the illegal address might relate to, since that's often useful. So, if it points into a block of memory which has already been freed, you'll be informed of this, and also where the block was freed. Likewise, if it should turn out to be just off the end of a heap block, a common result of off-by-one-errors in array subscripting, you'll be informed of this fact, and also where the block was allocated. If you use the --read-var-info option Memcheck will run more slowly but may give a more detailed description of any illegal address.";
